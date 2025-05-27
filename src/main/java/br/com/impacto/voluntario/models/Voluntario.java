77379dfb576afd94241ceba3e4a0dbf5b20d5d9f
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-05-27 18:08:12 -0300

    feat: add Voluntario entity

---
+++ src/main/java/br/com/impacto/voluntario/models/Voluntario.java
@@ -0,0 +1,31 @@
+package br.com.impacto.voluntario.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "voluntarios")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(of = "id")
+public class Voluntario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    @JsonIgnore
+    private String senha;
+    private Boolean ativo;
+    private LocalDate dataNascimento;
+    private LocalDate dataCadastro;
+    private Long necessidadesConcluidas;
+}
