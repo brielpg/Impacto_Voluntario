9f83aebcb4be14208e4023f35a2c128a22078b54
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-05-27 18:31:27 -0300

    feat: add create voluntario dto

---
+++ src/main/java/br/com/impacto/voluntario/dtos/CreateVoluntarioDto.java
@@ -0,0 +1,44 @@
+package br.com.impacto.voluntario.dtos;
+
+import br.com.impacto.voluntario.dtos.DtoEndereco;
+import br.com.impacto.voluntario.enums.HabilidadesEnum;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record CreateVoluntarioDto(
+        @NotBlank(message = "Nome é obrigatório")
+        String nome,
+
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Formato de email inválido")
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{11}", message = "CPF inválido, deve conter 11 dígitos")
+        String cpf,
+
+        @NotBlank(message = "Telefone é obrigatório")
+        String telefone,
+
+        @NotBlank(message = "Senha é obrigatória")
+        String senha,
+
+        @NotNull(message = "Data de nascimento é obrigatória")
+        LocalDate dataNascimento,
+
+        @NotNull(message = "Endereço é obrigatório")
+        @Valid
+        DtoEndereco endereco,
+
+        @NotBlank(message = "Habilidades sao obrigatórias")
+        List<HabilidadesEnum> habilidades,
+
+        String habilidadeOutro
+) {
+}
