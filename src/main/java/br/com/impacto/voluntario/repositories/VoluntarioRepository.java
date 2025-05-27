bff826e141f68c475c507c9c7173dff3f90d7ece
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-05-27 18:34:29 -0300

    feat: add voluntario repository

---
+++ src/main/java/br/com/impacto/voluntario/repositories/VoluntarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.impacto.voluntario.repositories;
+
+import br.com.impacto.voluntario.models.Voluntario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoluntarioRepository extends JpaRepository<Voluntario, Long> {
+}
