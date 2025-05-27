8d32fc5f7db5bb93091d3ba46f0a4f638e62a290
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-05-27 19:33:04 -0300

    feat: add templatesController

---
+++ src/main/java/br/com/impacto/voluntario/controllers/TemplatesController.java
@@ -0,0 +1,20 @@
+package br.com.impacto.voluntario.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class TemplatesController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/ps")
+    public String primeirosSocorros(){
+        return "primeirosSocorros";
+    }
+}
