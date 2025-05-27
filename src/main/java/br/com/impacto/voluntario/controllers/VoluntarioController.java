fc8673b59f00b5e7491df0194bf09c5c191320df
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-05-27 20:06:35 -0300

    feat: add VoluntarioController

---
+++ src/main/java/br/com/impacto/voluntario/controllers/VoluntarioController.java
@@ -0,0 +1,33 @@
+package br.com.impacto.voluntario.controllers;
+
+import br.com.impacto.voluntario.dtos.CreateVoluntarioDto;
+import br.com.impacto.voluntario.models.Voluntario;
+import br.com.impacto.voluntario.services.VoluntarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/voluntario")
+public class VoluntarioController {
+
+    @Autowired
+    private VoluntarioService service;
+
+    @GetMapping
+    public String createPage(Model model){
+        model.addAttribute("voluntario", new Voluntario());
+        return "voluntario/cadastro";
+    }
+
+    @PostMapping
+    public String createVoluntario(@Valid CreateVoluntarioDto dto, Model model){
+        var voluntario =  service.create(dto);
+        model.addAttribute("voluntario", voluntario);
+        return "redirect:home";
+    }
+}
