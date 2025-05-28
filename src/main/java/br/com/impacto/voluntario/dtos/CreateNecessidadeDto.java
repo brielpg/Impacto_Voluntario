d5d91be05acabfe37baf885f14913d5c175066d4
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-05-28 12:03:14 -0300

    feat: add CreateNecessidadeDto dto

---
+++ src/main/java/br/com/impacto/voluntario/dtos/CreateNecessidadeDto.java
@@ -0,0 +1,27 @@
+package br.com.impacto.voluntario.dtos;
+
+import br.com.impacto.voluntario.enums.DesastreEnum;
+import br.com.impacto.voluntario.enums.HabilidadesEnum;
+import br.com.impacto.voluntario.enums.UrgenciaEnum;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+public record CreateNecessidadeDto(
+        @NotBlank(message = "Titulo é obrigatório")
+        String titulo,
+        @NotNull(message = "Endereço é obrigatório")
+        @Valid
+        DtoEndereco endereco,
+        @NotNull(message = "Desastre é obrigatório")
+        DesastreEnum desastre,
+        @NotNull(message = "Urgencia é obrigatório")
+        UrgenciaEnum urgencia,
+        @NotEmpty(message = "Habilidades Requeridas sao obrigatórias")
+        List<HabilidadesEnum> habilidadesRequeridas,
+        String habilidadeOutro
+) {
+}
