e1fa5199d0f07af009b7663332f0b588661f5893
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-05-28 14:39:23 -0300

    feat: add query in method findAllByAtivo

--- src/main/java/br/com/impacto/voluntario/repositories/NecessidadeRepository.java
+++ src/main/java/br/com/impacto/voluntario/repositories/NecessidadeRepository.java
@@ -2,8 +2,13 @@ package br.com.impacto.voluntario.repositories;
 
 import br.com.impacto.voluntario.models.Necessidade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface NecessidadeRepository extends JpaRepository<Necessidade, Long> {
+    @Query("SELECT n FROM Necessidade n WHERE n.ativo = true")
+    List<Necessidade> findAllByAtivo();
 }
--- src/main/java/br/com/impacto/voluntario/repositories/SolicitacaoAjudaRepository.java
+++ src/main/java/br/com/impacto/voluntario/repositories/SolicitacaoAjudaRepository.java
@@ -2,11 +2,13 @@ package br.com.impacto.voluntario.repositories;
 
 import br.com.impacto.voluntario.models.SolicitacaoAjuda;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface SolicitacaoAjudaRepository extends JpaRepository<SolicitacaoAjuda, Long> {
+    @Query("SELECT s FROM SolicitacaoAjuda s WHERE s.ativo = true")
     List<SolicitacaoAjuda> findAllByAtivo();
 }
