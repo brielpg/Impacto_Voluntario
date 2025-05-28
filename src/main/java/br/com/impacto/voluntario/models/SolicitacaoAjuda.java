c322bf9a4a13158681de6b15e9c4a00a96c65f53
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-05-28 01:41:33 -0300

    feat: add SolicitacaoAjuda entity

---
+++ src/main/java/br/com/impacto/voluntario/models/SolicitacaoAjuda.java
@@ -0,0 +1,39 @@
+package br.com.impacto.voluntario.models;
+
+import br.com.impacto.voluntario.enums.AjudaRequeridaEnum;
+import br.com.impacto.voluntario.enums.DesastreEnum;
+import br.com.impacto.voluntario.enums.UrgenciaEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "voluntarios")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(of = "id")
+public class SolicitacaoAjuda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nomeSolicitante;
+    private String emailSolicitante;
+    private String telefoneSolicitante;
+    private String descricao;
+    private LocalDate dataEnvio;
+    private Boolean ativo;
+    private LocalDate dataAcontecimento;
+    private Integer pessoasAfetadas;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "endereco_id")
+    private Endereco endereco;
+    private DesastreEnum desastre;
+    private UrgenciaEnum urgencia;
+    private AjudaRequeridaEnum ajudaRequerida;
+    private String outros;
+}
