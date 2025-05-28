997d8e9bc8144c29bc22fc5b4ff6aa1ee7b08ceb
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-05-27 22:50:12 -0300

    refactor: refactor home GetMapping url

--- src/main/java/br/com/impacto/voluntario/controllers/TemplatesController.java
+++ src/main/java/br/com/impacto/voluntario/controllers/TemplatesController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class TemplatesController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home(){
         return "home";
     }
