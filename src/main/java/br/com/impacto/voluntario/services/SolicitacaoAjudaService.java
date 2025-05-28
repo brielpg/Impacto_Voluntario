54bce8eed0ac98240a54cf5f2662dcf11ad82839
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-05-28 03:07:02 -0300

    feat: add SolicitacaoAjudaService service

---
+++ src/main/java/br/com/impacto/voluntario/services/SolicitacaoAjudaService.java
@@ -0,0 +1,58 @@
+package br.com.impacto.voluntario.services;
+
+import br.com.impacto.voluntario.dtos.CreateSolicitacaoAjudaDto;
+import br.com.impacto.voluntario.enums.AjudaRequeridaEnum;
+import br.com.impacto.voluntario.models.Endereco;
+import br.com.impacto.voluntario.models.SolicitacaoAjuda;
+import br.com.impacto.voluntario.repositories.EnderecoRepository;
+import br.com.impacto.voluntario.repositories.SolicitacaoAjudaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@Service
+public class SolicitacaoAjudaService {
+
+    @Autowired
+    private SolicitacaoAjudaRepository repository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Transactional
+    public SolicitacaoAjuda create(CreateSolicitacaoAjudaDto dto){
+        if (dto.dataAcontecimento().isAfter(LocalDate.now())) throw new RuntimeException("Date cannot be higher than now");
+
+        var endereco = enderecoRepository.save(new Endereco(dto.endereco()));
+        var solicitacao = dtoToEntity(dto);
+
+        solicitacao.setEndereco(endereco);
+        solicitacao.setAtivo(true);
+        solicitacao.setDataEnvio(LocalDate.now());
+
+        repository.save(solicitacao);
+
+        return solicitacao;
+    }
+
+    private SolicitacaoAjuda dtoToEntity(CreateSolicitacaoAjudaDto dto){
+        var solicitacao = new SolicitacaoAjuda();
+
+        solicitacao.setNomeSolicitante(dto.nomeSolicitante());
+        solicitacao.setEmailSolicitante(dto.emailSolicitante());
+        solicitacao.setTelefoneSolicitante(dto.telefoneSolicitante());
+        solicitacao.setDescricao(dto.descricao());
+        solicitacao.setDataAcontecimento(dto.dataAcontecimento());
+        solicitacao.setPessoasAfetadas(dto.pessoasAfetadas());
+        solicitacao.setDesastre(dto.desastre());
+        solicitacao.setUrgencia(dto.urgencia());
+        solicitacao.setAjudaRequerida(dto.ajudaRequerida());
+
+        if (dto.ajudaRequerida().contains(AjudaRequeridaEnum.OUTROS))
+            solicitacao.setOutros(dto.outros());
+
+        return solicitacao;
+    }
+}
