b0b3d3d5986efd06b70da92f1e77503bb846b29f
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-05-30 23:22:43 -0300

    feat: Enable MethodSecurity and disable csrf

--- IMV-Monolito/src/main/java/br/com/impacto/voluntario/config/SecurityConfig.java
+++ IMV-Monolito/src/main/java/br/com/impacto/voluntario/config/SecurityConfig.java
@@ -2,26 +2,27 @@ package br.com.impacto.voluntario.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.List;
-
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/login", "/login/**", "/", "/primeirosSocorros", "/voluntario", "/ajuda").permitAll();
                     auth.requestMatchers("/admin", "/admin/**").hasRole("ADMIN");
